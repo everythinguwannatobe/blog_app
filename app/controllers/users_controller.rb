c9c6367ca3a3ee896052da37372a2c09f613aa8c
spencerduncanpower <timdonerpower@gmail.com>
2021-10-27 19:47:35 -0400

    update simplecov configuration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle a successful save
+      redirect_to @user
     else
       render :new
     end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require "simplecov"
 SimpleCov.start do
   add_filter "/spec/"
+  add_filter "/config/"
 end
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
