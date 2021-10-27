bd0d9fecc3ff2fef8ae08aa2942cd23dd9b37055
spencerduncanpower <timdonerpower@gmail.com>
2021-10-27 12:54:55 -0400

    Stub out invalid submission to new user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
+  def show
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # successful save
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
