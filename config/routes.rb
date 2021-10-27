f5523b5cb838204e227b7b5e22e3785e7c931a02
spencerduncanpower <timdonerpower@gmail.com>
2021-10-27 10:25:46 -0400

    Stub out sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'static#home'
   get 'static/home'
+  get '/signup', to: 'users#new'
+
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
